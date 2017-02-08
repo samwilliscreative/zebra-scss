22f6fdc7ec71580b355cd4c7fa88fdb02cb2c918
Sam Willis <sam.willis@senior.co.uk>
2017-02-08 15:16:39 +0000

    Addedd CSSNANO as a package

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
 
     gulp.task('sass', function () {
     return gulp.src('./src/sass/zebra.scss')
 
-    .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie >= 8'],
             cascade: false
         }))
+        .pipe(cssnano())
         .pipe(sourcemaps.write('.'))
-
         .pipe(gulp.dest('./dist/css/'));
     });
 
     gulp.task('default', function () {
         gulp.watch('./src/sass/*.scss', ['sass']);
+
     });
