68b1ee605cf9aaae808ed46dffb450d18100f7ad
Sam Willis <swillis93@googlemail.com>
2017-02-08 23:40:21 +0000

    Added minification to the project, removed sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
 var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 
     gulp.task('sass', function () {
     return gulp.src('./src/sass/zebra.scss')
 
-        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie >= 8'],
             cascade: false
         }))
-        .pipe(cssnano())
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/'));
     });
 
+    gulp.task('minify', function(){
+        gulp.src('./dist/css/zebra.css')
+            .pipe(cssnano())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+        .pipe(gulp.dest('./dist/css/'));
+    })
+
     gulp.task('default', function () {
-        gulp.watch('./src/sass/*.scss', ['sass']);
+        gulp.watch('./src/sass/*.scss', ['sass', 'minify']);
 
     });
